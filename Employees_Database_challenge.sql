fa43378eaaea58d3866364ceef5477ff2d0746f1
Terra Lasho <96261773+Beetleee@users.noreply.github.com>
2022-02-19 10:45:32 -0600

    Add files via upload

--- Employees_Database_challenge.sql
+++ Employees_Database_challenge.sql
@@ -12,11 +12,41 @@ FROM employees as e
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
--- Deliverable 1b: DISTINCT ON to remove duplicate rows
+
+-- Deliverable 1b: retrieve the first occurance 
+-- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
+    first_name,
+    last_name,
+    title
 INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, title DESC;
+
+-- Deliverable 1c: retrieve employees by their most recent job title 
+-- who are about to retire.
+SELECT COUNT(un.emp_no),
+un.title
+INTO retiring_titles
+FROM unique_titles as un
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+
+-- DELIVERABLE 2:
+-- create a Mentorship Eligibility table 
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
